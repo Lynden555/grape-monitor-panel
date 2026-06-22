1797879d9971d8d6858079823c6cc4cb9a221cea
Lynden555 <diegollera1@gmail.com>
2026-06-22 16:29:53 -0700

    feat: Nuevo Agente 2.9.7

--- src/components/EmpresasPanel/modals/DownloadAgentModal.jsx
+++ src/components/EmpresasPanel/modals/DownloadAgentModal.jsx
@@ -5,7 +5,7 @@ import {
 } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 
-const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/2.9.5/Grape.Monitor.Setup.2.9.5.exe';
+const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/2.9.7/Grape.Monitor.Setup.2.9.7.exe';
 
 ////const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/v2.5.0/Grape.Monitor.Setup.2.9.4.exe';
 
