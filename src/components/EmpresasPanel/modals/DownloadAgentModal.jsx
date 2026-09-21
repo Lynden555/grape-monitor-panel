07e5798fead1e88f660e90b9986280442603334c
Lynden555 <diegollera1@gmail.com>
2026-09-21 09:57:08 -0700

    Regreso SNPM VieJo

--- src/components/EmpresasPanel/modals/DownloadAgentModal.jsx
+++ src/components/EmpresasPanel/modals/DownloadAgentModal.jsx
@@ -5,9 +5,9 @@ import {
 } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 
-const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grape-monitor-agent/releases/download/v3.3.2/Grape-Monitor-Setup-3.3.2.exe';
+///const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grape-monitor-agent/releases/download/v3.3.2/Grape-Monitor-Setup-3.3.2.exe';
 
-////const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/v3.1.0/Grape.Monitor.Setup.3.1.0.exe';
+const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/v3.1.0/Grape.Monitor.Setup.3.1.0.exe';
 ////const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/2.9.7/Grape.Monitor.Setup.2.9.7.exe';
 ////const AGENT_DOWNLOAD_URL = 'https://github.com/Lynden555/grapeassist-software/releases/download/v2.5.0/Grape.Monitor.Setup.2.9.4.exe';
 
